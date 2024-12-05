c614d466533e228c88befced1bfc224a56dca73c
zImfastr <max@schrader.team>
2024-12-05 16:08:02 +0100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![allow(unused_variables)] #![allow(unused_imports)]
+
+/*
+
+This is only a learning Project. It's only for me to learn to work with OpenGL
+I just want to make my result, an animated 3d Cube, public. It's not going to be a
+revolutionary Project in any way or form, as I said it's only for learning.
+This dev branch has currently nothing to do with the finished "Project" as I don't
+even know how to use OpenGL.
+
+*/
+
+use std::io;
+use std::io::stdin;
+
+fn main() {
+
+}
